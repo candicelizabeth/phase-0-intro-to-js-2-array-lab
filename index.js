6524b46736d1a3d582136468e682a948bf89d953
Candice Peters <candice.peters@flatironschool.com>
2021-04-11 13:40:28 -0500

    commit tree 80dd208b5a2220e83574d0c760a2260ef10dffe6

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+   return cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+   return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+function appendCat(name){
+   let newCats = [...cats, name]
+    return newCats
+}
+
+function prependCat(name){
+    let newCats = [name, ...cats]
+    return newCats
+}
+
+function removeLastCat(){
+    return cats.slice(0,- 1) //slice is not destructive 
+    // let newCats = [...cats]
+    // // first make copy 
+    // newCats.pop() //returns the removed item
+    // //remove value
+    // //return array 
+    // return newCats
+}
+
+function removeFirstCat(){
+    return cats.slice(1) // slice is not destructive
+    // let newCats = [...cats]
+    // newCats.shift() //returns the removed item
+    // return newCats
+}
